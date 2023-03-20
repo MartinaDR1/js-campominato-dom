11c73e966dcd28f3d081c6f24df762193baf8028
MartinaDR1 <derosemartina933@gmail.com>
2023-03-20 14:07:10 +0100

    generate numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,16 @@ const footerEl = document.querySelector('footer')
 
 btnSend.addEventListener('click', function(){
     containerEl.innerHTML = '';
-    
 
     if(selectEl.value == 'easy'){
         cellContainer (100, 'easy')
+        random (100)
     } else if (selectEl.value == 'medium'){
         cellContainer(81, 'medium')
+        random (81)
     } else {
         cellContainer (49, 'hard')
+        random (49)
     }
 
     footerEl.innerHTML = "<span> Made with &hearts; by Boolean</span>";
@@ -57,3 +59,42 @@ function cellContainer (maxCells, difficulty){
     
 }
 
+/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella:
+se il numero è presente nella lista dei numeri generati
+abbiamo calpestato una bomba
+la cella si colora di rosso e la partita termina.
+Altrimenti
+la cella cliccata si colora di azzurro
+l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.*/
+
+// Creo un array vuoto
+let bomb = [];
+
+// Funzione per i numeri casuali
+function pcRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+// Ciclo i numeri in una funzione per generare le 16 bombe
+
+function random (maxCells){
+    let i = 1;
+
+    while (i <= 16) {
+
+        let randomNumb = pcRandom(1, maxCells); 
+
+        if (!bomb.includes(randomNumb)) {
+            bomb.push(randomNumb);
+        } else {
+            continue;
+        }
+        i++;
+    }
+
+    console.log(bomb);
+}
